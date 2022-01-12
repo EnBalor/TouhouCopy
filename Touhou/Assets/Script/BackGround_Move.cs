5088199744e0940476bd2f5f94e73c196cccc228
00GOEUN <enbalor4334@gmail.com>
2022-01-12 17:48:58 +0900

    Background/UI

---
+++ Touhou/Assets/Script/BackGround_Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround_Move : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _backGround = null;
+    Renderer _renderer;
+
+    float _speed = 5f;
+    float _offset;
+
+
+    void Start()
+    {
+        _renderer = _backGround.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        _offset = _speed * Time.deltaTime;
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(0, _offset));
+    }
+}
