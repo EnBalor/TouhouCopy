65c7f85e80dfbff53a4b221957a64cf1582b1a36
EnBalor <enbalor4334@gmail.com>
2022-01-27 22:59:45 +0900

    third

--- Touhou/Assets/Script/Enemy/Middle_Boss/Third_knife.cs
+++ Touhou/Assets/Script/Enemy/Middle_Boss/Third_knife.cs
@@ -9,7 +9,7 @@ public class Third_knife : MonoBehaviour
 
     float _movetime = 2.5f;
 
-    float _chaser = 3f;
+    float _chaser = 1f;
     float _chaserspeed = 1f;
 
     Reimu _rm;
@@ -17,15 +17,30 @@ public class Third_knife : MonoBehaviour
     void Start()
     {
         _rm = GameObject.Find("Player").GetComponent<Reimu>();
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+        Vector3 dir = target.transform.position - this.transform.position;
+
+        if (_chaser <= 0)
+        {
+            _speed = 0;
+
+            if(target != null)
+            {
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+                Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, _rotspeed * Time.deltaTime);
+                transform.rotation = rotation;
+
+                _chaser = 1f;
+            }
+        }
     }
 
     void Update()
     {
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
-        Vector3 dir = Vector3.down;
-
         _chaser -= Time.deltaTime;
         _movetime -= Time.deltaTime;
+        Debug.Log(_chaser);
 
         if (_chaser >= 0)
         {
@@ -34,25 +49,7 @@ public class Third_knife : MonoBehaviour
 
         else if(_chaser <= 0)
         {
-            _speed = 0f;
-
-            if (target != null)
-            {
-
-                dir = new Vector3(this.transform.position.x - target.transform.position.x, this.transform.position.y - target.transform.position.y, 0);
-
-                transform.Translate(dir * _chaserspeed * Time.deltaTime);
-
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
-                Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, _rotspeed * Time.deltaTime);
-                transform.rotation = rotation;
-            }
-
-            else
-            {
-                transform.Translate(dir * _speed * Time.deltaTime, Space.Self);
-            }
+            transform.Translate(dir * _chaserspeed * Time.deltaTime);
         }
 
         if (_rm._clearBullet == true)
