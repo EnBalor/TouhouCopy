4326937fcb85ff521655082142e1ce02892c188c
00GOEUN <enbalor4334@gmail.com>
2022-01-28 17:47:54 +0900

    third

--- Touhou/Assets/Script/Enemy/Middle_Boss/Third_knife.cs
+++ Touhou/Assets/Script/Enemy/Middle_Boss/Third_knife.cs
@@ -9,7 +9,7 @@ public class Third_knife : MonoBehaviour
 
     float _movetime = 2.5f;
 
-    float _chaser = 1f;
+    float _chaser = 4f;
     float _chaserspeed = 1f;
 
     Reimu _rm;
@@ -17,22 +17,13 @@ public class Third_knife : MonoBehaviour
     void Start()
     {
         _rm = GameObject.Find("Player").GetComponent<Reimu>();
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
-        Vector3 dir = target.transform.position - this.transform.position;
+        
 
         if (_chaser <= 0)
         {
-            _speed = 0;
+            //_speed = 0;
 
-            if(target != null)
-            {
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
-                Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, _rotspeed * Time.deltaTime);
-                transform.rotation = rotation;
-
-                _chaser = 1f;
-            }
+            
         }
     }
 
@@ -49,7 +40,26 @@ public class Third_knife : MonoBehaviour
 
         else if(_chaser <= 0)
         {
-            transform.Translate(dir * _chaserspeed * Time.deltaTime);
+            GameObject target = GameObject.FindGameObjectWithTag("Player");
+            Vector3 dir = target.transform.position - this.transform.position;
+
+            if (target != null)
+            {
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                Quaternion angleAxis = Quaternion.AngleAxis(angle - 180f, Vector3.forward);
+                Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, _rotspeed * Time.deltaTime);
+                transform.rotation = rotation;
+
+                _chaser = 1f;
+
+                transform.Translate(dir * _chaserspeed * Time.deltaTime);
+
+            }
+
+            else
+            {
+                transform.Translate(Vector3.down * _speed * Time.deltaTime, Space.Self);
+            }
         }
 
         if (_rm._clearBullet == true)
